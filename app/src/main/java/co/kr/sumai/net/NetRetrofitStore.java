22fff7d7e3b336695988a4ba1836cb9be8fe3a07
UxU-Inc <ibada0928@gmail.com>
2021-02-19 18:48:45 +0900

    admob add, summaryRequest add

--- app/src/main/java/co/kr/sumai/net/NetRetrofitStore.java
+++ app/src/main/java/co/kr/sumai/net/NetRetrofitStore.java
@@ -1,5 +1,8 @@
 package co.kr.sumai.net;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,9 +14,16 @@ public class NetRetrofitStore {
     private NetRetrofitStore() {
     }
 
+    OkHttpClient okHttpClient = new OkHttpClient.Builder()
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .build();
+
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("https://www.sumai.co.kr")
             .addConverterFactory(GsonConverterFactory.create()) // 파싱등록
+            .client(okHttpClient)
             .build();
 
     SumaiService service = retrofit.create(SumaiService.class);
