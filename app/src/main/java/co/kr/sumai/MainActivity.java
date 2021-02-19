90bc913ecadc997f34221c9939a7a40ac993cb86
UxU-Inc <ibada0928@gmail.com>
2021-02-19 18:48:45 +0900

    admob add, summaryRequest add

--- app/src/main/java/co/kr/sumai/MainActivity.java
+++ app/src/main/java/co/kr/sumai/MainActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,12 +26,17 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
@@ -54,13 +60,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     EditText editTextSummary;
     TextView textViewSummaryResult;
 
+    TextView textViewLimitGuide;
+
+    FrameLayout layoutLoading;
+
     private FrameLayout adContainerView;
-    private AdView adView; // 애드몹 배너
+    private AdView adView; // 애드몹 배너 광고
+    private InterstitialAd mInterstitialAd; // 애드몹 전면 광고
+
+    private String adMobBannerID;
+    private String adMobInterstitialID;
 
     private FirebaseAnalytics mFirebaseAnalytics;
 
     String ID = "";
-    int record = 1;
+    private int record = 1;
+
+    private int summaryRequestCount = 0;
 
     //뒤로 버튼 두번 연속 클릭 시 종료
     private long time= 0;
@@ -88,16 +104,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         // AdMob
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) { }
-        });
+        AdmobInit();
 
-        adContainerView = findViewById(R.id.ad_view_container);
-        adView = new AdView(this);
-        adView.setAdUnitId(getString(R.string.adaptive_banner_ad_unit_id));
-        adContainerView.addView(adView);
-        loadBanner();
     }
 
     private void initLayout() {
@@ -147,6 +155,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 if(0 < editTextSummary.getText().toString().length()) {
                     if(imageButtonClear.getVisibility() == View.INVISIBLE)
                         imageButtonClear.setVisibility(View.VISIBLE);
+
+                    if(5000 <= editTextSummary.getText().toString().length())
+                        textViewLimitGuide.setVisibility(View.VISIBLE);
                 }
                 else {
                     textViewSummaryResult.setVisibility(View.INVISIBLE);
@@ -168,6 +179,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         textViewSummaryResult = (TextView) findViewById(R.id.textViewSummaryResult);
         textViewSummaryResult.setVisibility(View.INVISIBLE);
+
+        textViewLimitGuide = (TextView) findViewById(R.id.textViewLimitGuide);
+        textViewLimitGuide.setVisibility(View.INVISIBLE);
+
+        layoutLoading = (FrameLayout) findViewById(R.id.layoutLoading);
+        layoutLoading.setVisibility(View.INVISIBLE);
     }
 
     @Override
@@ -195,27 +212,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Intent intent;
         switch (item.getItemId()) {
             case R.id.home:
-//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                intent = new Intent(getApplicationContext(), MainActivity.class);
 //                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 //                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 //                startActivity(intent);
                 break;
             case R.id.terms:
-//                intent = new Intent(getApplicationContext(), GuideActivity.class);
-//                intent.putExtra("tabs", 1);
-//                startActivity(intent);
+                intent = new Intent(getApplicationContext(), GuideActivity.class);
+                intent.putExtra("page", 0);
+                startActivity(intent);
                 break;
             case R.id.privacy:
-//                intent = new Intent(getApplicationContext(), GuideActivity.class);
-//                intent.putExtra("tabs", 2);
-//                startActivity(intent);
+                intent = new Intent(getApplicationContext(), GuideActivity.class);
+                intent.putExtra("page", 1);
+                startActivity(intent);
                 break;
             case R.id.notice:
-//                intent = new Intent(getApplicationContext(), GuideActivity.class);
-//                intent.putExtra("tabs", 3);
-//                startActivity(intent);
+                intent = new Intent(getApplicationContext(), GuideActivity.class);
+                intent.putExtra("page", 2);
+                startActivity(intent);
                 break;
             case R.id.sendFeedback:
                 sendMail();
@@ -241,13 +259,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         layoutLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this, LoginActivity);
-//                startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
 
     private void summaryRequest(String data) {
+
+        layoutLoading.setVisibility(View.VISIBLE);
+        layoutLoading.setClickable(true);
+
+        if (mInterstitialAd != null && 4 <= summaryRequestCount) {
+            mInterstitialAd.show(this);
+        }
+        loadInterstitial();
 
         Call<SummaryResponse> res = NetRetrofitStore.getInstance().getService().SUMMARY(new SummaryRequest(data, ID, record));
         res.enqueue(new Callback<SummaryResponse>() {
@@ -255,21 +281,52 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             public void onResponse(Call<SummaryResponse> call, Response<SummaryResponse> response) {
                 textViewSummaryResult.setText(response.body().getSummarize());
                 textViewSummaryResult.setVisibility(View.VISIBLE);
+                layoutLoading.setVisibility(View.INVISIBLE);
+                layoutLoading.setClickable(false);
+
+                summaryRequestCount++;
             }
 
             @Override
             public void onFailure(Call<SummaryResponse> call, Throwable t) {
-                Toast.makeText(getApplicationContext(), call + "\n" + t, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+                layoutLoading.setVisibility(View.INVISIBLE);
+                layoutLoading.setClickable(false);
             }
         });
 
     }
 
-    private void loadBanner() {
+    private void AdmobInit() {
+        if (BuildConfig.DEBUG) {
+            adMobBannerID = getString(R.string.adaptive_banner_ad_unit_id_test);
+            adMobInterstitialID = getString(R.string.adaptive_interstitial_ad_unit_id_test);
+        }
+        else {
+            adMobBannerID = getString(R.string.adaptive_banner_ad_unit_id);
+            adMobInterstitialID = getString(R.string.adaptive_interstitial_ad_unit_id);
+        }
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) { }
+        });
+
+
+        //┌────────────────────────────── 배너 광고 ──────────────────────────────┐
+        adContainerView = findViewById(R.id.ad_view_container);
+        adView = new AdView(this);
+        adView.setAdUnitId(adMobBannerID);
+        adContainerView.addView(adView);
+
         AdRequest adRequest = new AdRequest.Builder().build();
         AdSize adSize = getAdSize();
         adView.setAdSize(adSize);
         adView.loadAd(adRequest);
+
+        //┌────────────────────────────── 전면 광고 ──────────────────────────────┐
+        loadInterstitial();
+
     }
 
     private AdSize getAdSize() {
@@ -282,6 +339,42 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         int adWidth = (int) (widthPixels / density);
         return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
+    }
+
+    private void loadInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+
+        InterstitialAd.load(this, adMobInterstitialID, adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                // 광고 로딩
+                mInterstitialAd = interstitialAd;
+                mInterstitialAd.setFullScreenContentCallback(new FullScreenContentCallback(){
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        // 전면 광고가 닫힐 때 호출
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                        // 전면 광고가 나오지 않았올 때 호출
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        // 전면 광고가 나왔올 때 호출
+                        mInterstitialAd = null;
+                        summaryRequestCount = 0;
+                    }
+                });
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                // 오류 처리
+                mInterstitialAd = null;
+            }
+        });
     }
 
     private void sendMail() {
