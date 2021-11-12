e0f651ba03a0b11e8ec3fb93b40a417f950cef15
UxU-Inc <ibada0928@gmail.com>
2021-11-12 19:45:59 +0900

    Caii dev2

---
+++ app/src/main/java/co/kr/sumai/caii/CaiiMainActivity.kt
@@ -0,0 +1,11 @@
+package co.kr.sumai.caii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaiiMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_caii_main)
+    }
+}
