2195ec74b1967f13c5a1843074e9b67c05978073
UxU-Inc <ibada0928@gmail.com>
2021-12-02 21:40:50 +0900

    Caii UI develop

---
+++ app/src/main/java/co/kr/sumai/caii/CaiiCallingActivity.kt
@@ -0,0 +1,11 @@
+package co.kr.sumai.caii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaiiCallingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_caii_calling)
+    }
+}
